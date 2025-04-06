863fb929ba2604c719be56ed70f61cfd23648134
Mauricio Perez <mauricio.perez24@itca.edu.sv>
2025-04-06 10:20:41 -0600

    feat: insert categorrias

---
+++ controllers/CategoriasController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace controllers;
+
+use models\Categorias;
+use libs\Respuesta;
+use libs\Mensajes;
+
+class CategoriasController
+{
+
+  public function index()
+  {
+    require_once 'views/categorias/index.php';
+  }
+
+  public function insert()
+  {
+    //header('Content-Type: application/json; charset=UTF-8');
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      
+      $categoria = $_POST['categoria'];
+      try {
+        $Categorias = new Categorias();
+        $Categorias->insert($categoria);
+        return json_encode([
+          'status' => 'success',
+          'message' => 'Categoria registrada correctamente'
+        ]);
+        
+        exit; 
+      } catch (\Throwable $th) {
+        return json_encode(array(
+          'status' => 'error',
+          'message' => 'Error al registrar la categoria: ' . $th->getMessage()
+        ));
+        exit; 
+      }
+    }
+  }
+}
